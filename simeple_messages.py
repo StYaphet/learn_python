1ea0bc9d0537f480641daa7354ac4e49e0d6b1aa
StYaphet <947670170@qq.com>
2017-09-03 00:46:30 +0800

    简单消息打印

---
+++ simeple_messages.py
@@ -0,0 +1,5 @@
+#多条简单消息打印
+message = "message_1"
+print (message)
+message = "message_2"
+print (message)
---
+++ simple_message.py
@@ -0,0 +1,3 @@
+#2-1 简单消息的打印
+simple_message = "I, simple message!"
+print (simple_message)
