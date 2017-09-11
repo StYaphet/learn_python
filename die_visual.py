44474f4a34ca03fab9b01b7898f10b65586e7a0f
StYaphet <947670170@qq.com>
2017-09-11 23:53:23 +0800

    掷两个骰子和的直方图

--- die_visual.py
+++ die_visual.py
@@ -1,17 +1,19 @@
 from die import Die
 import pygal
 
-die = Die()
+die_1 = Die()
+die_2 = Die(10)
 
 results = []
-for roll_num in range(1000):
-	result = die.roll()
+for roll_num in range(50000):
+	result = die_1.roll() + die_2.roll()
 	results.append(result)
 
 # print(results)
 
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
 	frequency = results.count(value)
 	frequencies.append(frequency)
 
@@ -19,12 +21,12 @@ for value in range(1, die.num_sides+1):
 # 对数据结果进行可视化，为了创建条形图，我们创建了一个pygal.Bar()实例，并将其储存在hist中。
 hist = pygal.Bar()
 # 接下来，设置hist的属性title，将掷D6骰子的可能结果用作x轴的标签，并给每个轴都添加了标题
-hist.title = "Results of rolling one D6 1000 times."
-hist.x_labels = [1, 2, 3, 4, 5, 6]
+hist.title = "Results of rolling two D6 1000 times."
+hist.x_labels = list(range(2,17))
 hist.x_title = "Result" 
 hist.y_title = "Frequency of Result"
 # 使用add()将一系列值添加到图表中（向它传递要给添加的值指定的标签，还有一个列表，其中包含了将要出现在图标中的值）
-hist.add("D6", frequencies)
+hist.add("D6 + D10", frequencies)
 # 最后我们将这个图标渲染成为一个svg文件，这种文件的扩展名必须为.svg
 hist.render_to_file("die_visual.svg")
 
