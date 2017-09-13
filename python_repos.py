155aadf22b57deb9bda94510468859520f82d02d
StYaphet <947670170@qq.com>
2017-09-13 23:17:03 +0800

    可视化最受欢迎的obj-c仓库的信息

--- python_repos.py
+++ python_repos.py
@@ -1,5 +1,8 @@
 # 导入模块requests
 import requests
+# 导入pygal以及要应用于图表的pygal样式
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 # 储存API调用的URL
 url = "https://api.github.com/search/repositories?q=language:objective-c&sort=stars"
 # 然后使用requests来执行，我们调用get()并将URL传递给它，再将响应对象存储在变量r中
@@ -22,20 +25,34 @@ print("\nKeys:",len(repo_dict))
 # 答应这个字典的所有键，看看其中包含了哪些信息
 # for key in sorted(repo_dict.keys()):
 # 	print(key)
+names, stars = [], []
 
+# 获取数据
 for repo_dict in repo_dicts:
-	print("\nSelected information about first repository:") 
-	print('Name:', repo_dict['name'])
-	print('Owner:', repo_dict['owner']['login'])
-	print('Stars:', repo_dict['stargazers_count'])
-	print('Repository:', repo_dict['html_url']) 
-	print('Created:', repo_dict['created_at']) 
-	print('Updated:', repo_dict['updated_at'])
-	print('Description:', repo_dict['description'])
+	names.append(repo_dict["name"])
+	stars.append(repo_dict["stargazers_count"])
 
-
-
-
+# 可视化
+# 使用LightenStyle类(别名LS)定义了一种样式，并将其基色设置为深蓝色
+# 我们还传递了实参base_style，以使用LightColorizedStyle类(别名LCS)
+mystyle = LS("#333366", base_style=LCS)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+# 我们使用Bar() 创建一个简单的条形图，并向它传递了my_style 
+# 还传递了另外两个样式实参:让标签绕  轴旋转45度 (x_label_rotation=45 )，并隐藏了图例(show_legend=False )
+chart = pygal.Bar(my_config, style=mystyle)
+chart.title = "Most-Starred Objective-C Projects on GitHub"
+# 将属性x_labels 设置为列表names 。
+chart.x_labels = names
+# 由于我们不需要给这个数据系列添加标签，因此在此处添加数据时，将标签设置成了空字符串。
+chart.add("", stars)
+chart.render_to_file("python_repos.svg")
 
 
 
