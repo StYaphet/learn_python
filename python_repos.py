438c2aaaf792adf5946519b6ab4c9533305bb32e
StYaphet <947670170@qq.com>
2017-09-13 23:00:10 +0800

    打印最受欢迎的obj-c仓库的信息

--- python_repos.py
+++ python_repos.py
@@ -9,5 +9,35 @@ print("Status code:", r.status_code)
 
 # 将API响应存储在一个变量中，因为这个api返回json格式的信息，因此我们使用方法json()将这些信息转为一个python字典
 response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
+# 打印了与'total_count' 相关联的值，它指出了GitHub总共包含多少个Objective-C仓库。
+repo_dicts = response_dict["items"]
+print("Repositories returned:", len(repo_dicts)) 
 
-print(response_dict.keys())
+# 提取了repo_dicts 中的第一个字典，并将其存储在repo_dict 中
+repo_dict = repo_dicts[0]
+# 接下来，我们打印这个字典包含的键数，看看其中有多少信息
+print("\nKeys:",len(repo_dict))
+
+# 答应这个字典的所有键，看看其中包含了哪些信息
+# for key in sorted(repo_dict.keys()):
+# 	print(key)
+
+for repo_dict in repo_dicts:
+	print("\nSelected information about first repository:") 
+	print('Name:', repo_dict['name'])
+	print('Owner:', repo_dict['owner']['login'])
+	print('Stars:', repo_dict['stargazers_count'])
+	print('Repository:', repo_dict['html_url']) 
+	print('Created:', repo_dict['created_at']) 
+	print('Updated:', repo_dict['updated_at'])
+	print('Description:', repo_dict['description'])
+
+
+
+
+
+
+
+
+
